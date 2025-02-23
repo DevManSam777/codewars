5c1b0cee27f0f2ac81fd868079a8239943d8ef8e
Sam S. <devmansam@protonmail.com>
2025-02-23 14:48:47 -0800

    Add solution - CodeHub

---
+++ 8-kyu/the-wide-mouthed-frog/the-wide-mouthed-frog.js
@@ -0,0 +1,6 @@
+// convert animal string to lower case
+// if animal is "alligartor" return "small"
+// else return "wide"
+​
+  const mouthSize = (animal) => animal.toLowerCase() === "alligator" ? "small" : "wide";
+  
