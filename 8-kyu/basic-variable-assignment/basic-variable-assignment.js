76134d72ac4aec512c9f60cfe4badb2d37c730e3
Sam S. <devmansam@protonmail.com>
2025-02-24 17:13:54 -0800

    Add solution - CodeHub

---
+++ 8-kyu/basic-variable-assignment/basic-variable-assignment.js
@@ -0,0 +1,7 @@
+// var a == "code";
+// var b == "wa.rs";
+// var name == a + b;
+​
+// all variables are using the equality operator (==) instead of the assignment operator (=)
+​
+var a = "code", b = "wa.rs", name = a + b;
