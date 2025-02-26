01888ff6816ca76e694b9ae363736f00161e56c0
Sam S. <devmansam@protonmail.com>
2025-02-26 15:36:04 -0800

    Add solution - CodeHub

---
+++ 8-kyu/sentence-smash/sentence-smash.js
@@ -0,0 +1,6 @@
+function smash (words) {
+   // convert array of words into a string 
+  // with each word seperated by a space 
+  // use join with a single whitespace seperator 
+    return words.join(" ");
+};
