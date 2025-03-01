c5c92bdff72fd51e3f553f6058f2becf343f20b5
Sam S. <devmansam@protonmail.com>
2025-02-28 20:47:20 -0800

    Add solution - CodeHub

---
+++ 8-kyu/no-loops-2-you-only-need-one/no-loops-2-you-only-need-one.js
@@ -0,0 +1,4 @@
+// use the built in indexOf method to see if the value of x is found in the a array
+// if it is not found, indexOf will be -1
+​
+const check = (a,x) => a.indexOf(x) != -1 ? true : false
