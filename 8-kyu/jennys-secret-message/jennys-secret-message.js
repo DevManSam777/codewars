a555726b34dda58a0c9433cf34da46026de7977c
Sam S. <devmansam@protonmail.com>
2025-02-28 20:36:59 -0800

    Add solution - CodeHub

---
+++ 8-kyu/jennys-secret-message/jennys-secret-message.js
@@ -0,0 +1,11 @@
+function greet(name){
+// default return statement should be under johnny message 
+// if statement should be declaration should be in square brackets
+//   return "Hello, " + name + "!";
+//   if(name === "Johnny")
+//     return "Hello, my love!";
+  
+  if(name === "Johnny") {
+    return "Hello, my love!";
+  } return `Hello, ${name}!`;
+}
