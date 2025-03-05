7392c6ef1046a8e58ac09ebd5504ea7a461c2de8
Sam S. <devmansam@protonmail.com>
2025-03-05 02:58:41 -0800

    Add solution - CodeHub

---
+++ 8-kyu/determine-offspring-sex-based-on-genes-xx-and-xy-chromosomes/determine-offspring-sex-based-on-genes-xx-and-xy-chromosomes.js
@@ -0,0 +1,13 @@
+function chromosomeCheck(gamete) {
+  // if sperm is y 
+  // return male string
+  // else return female string
+  
+  return gamete === 'XY' 
+    ? "Congratulations! You're going to have a son."
+    : "Congratulations! You're going to have a daughter."
+}
+​
+​
+​
+​
