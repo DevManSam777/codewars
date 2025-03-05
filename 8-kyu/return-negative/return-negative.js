597cc94e9f9a556b19943b4ef4fcd0f332646747
Sam S. <devmansam@protonmail.com>
2025-03-05 03:00:45 -0800

    Add solution - CodeHub

---
+++ 8-kyu/return-negative/return-negative.js
@@ -0,0 +1,8 @@
+function makeNegative(num) {
+  // return
+  // ternary operation: condition ? if true : false
+  // num greater than 0
+  // num is positive multiply it by -1 to make it negative
+  // num is negative return num
+  return num > 0 ? num * -1 : num
+}
