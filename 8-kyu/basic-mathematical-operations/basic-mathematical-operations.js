ecd4cc6b2b21b6890f76f22be93ded0504530095
Sam S. <devmansam@protonmail.com>
2025-03-05 14:18:22 -0800

    Update solution - CodeHub

--- 8-kyu/basic-mathematical-operations/basic-mathematical-operations.js
+++ 8-kyu/basic-mathematical-operations/basic-mathematical-operations.js
@@ -1,6 +1,4 @@
-function basicOp(operation, value1, value2){
-  // switch statement with case being operation
-  let val = 0;
+ function basicOp(operation, value1, value2){
   switch (operation) {
       case "+":
         val = value1 + value2;
@@ -18,8 +16,22 @@ function basicOp(operation, value1, value2){
         break;
     }
   return val;
-}
+};
 ​
 // const basicOp = (o, v1, v2) => {
 //  return o === "+" ? v1 + v2 : o === "-" ? v1 - v2 : o === "*" ? v1 * v2 : o === "/" ? v1 / v2 : 0;
+// }
+​
+​
+// function basicOp(operation, value1, value2){
+//   // the operation argument determins the math operation
+//   // create an object with operations as keys and callbacks as values
+//   const ops = {
+//     "+": (a,b) => a + b,
+//     "-": (a,b) => a - b,
+//     "*": (a,b) => a * b,
+//     "/": (a,b) => a / b
+//   }
+//   // return a string literal 
+//   return `${value1} ${operation} ${value2} = ${ops[operation](value1, value2)}`;
 // }
