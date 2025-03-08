6f202c082afaf2adae1cec9e7d5ff87e4a3bcebc
Sam S. <devmansam@protonmail.com>
2025-03-08 11:56:13 -0800

    Add solution - CodeHub

---
+++ 8-kyu/basic-making-six-toast/basic-making-six-toast.js
@@ -0,0 +1,15 @@
+function sixToast(num) {
+  // return absolute value
+  // if num is less than 6
+  return Math.abs(num < 6  
+  // subtract num from 6
+                  ? 6 - num
+  // else if num is more than 6 
+                  : num > 6
+  // subtract 6 from num
+                  ? num - 6
+  // else if num is equal to 6 
+  // 0
+                  : 0
+                  )
+}
