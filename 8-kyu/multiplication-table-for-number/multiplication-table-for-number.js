b3cef46b07e50416136c881bb89541f314d64133
Sam S. <devmansam@protonmail.com>
2025-03-09 18:35:52 -0700

    Add solution - CodeHub

---
+++ 8-kyu/multiplication-table-for-number/multiplication-table-for-number.js
@@ -0,0 +1,5 @@
+function multiTable(n) {
+  // use a string literal to return a multiplication table from 1 - 10 for n
+  return `1 * ${n} = ${n}\n2 * ${n} = ${2 * n}\n3 * ${n} = ${3 * n}\n4 * ${n} = ${4 * n}\n5 * ${n} = ${5 * n}\n6 * ${n} = ${6 * n}\n7 * ${n} = ${7 * n}\n8 * ${n} = ${8 * n}\n9 * ${n} = ${9 * n}\n10 * ${n} = ${10 * n}`
+  
+}
