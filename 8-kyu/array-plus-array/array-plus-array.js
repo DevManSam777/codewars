997c07b8c676f6f0265e8fbbc859ee7523831b91
Sam S. <devmansam@protonmail.com>
2025-03-09 18:10:25 -0700

    Update solution - CodeHub

--- 8-kyu/array-plus-array/array-plus-array.js
+++ 8-kyu/array-plus-array/array-plus-array.js
@@ -1,5 +1,4 @@
 function arrayPlusArray(arr1, arr2) {
-  // use reduce method to accumulate sums of each array
-  // return sum of accumulated arr1 indices + sum of accumulated arr2 indices
-  return arr1.reduce((acc, num) => acc + num) + arr2.reduce((acc, num) => acc + num); //something went wrong
+  // return spread array reduced to accumulate sum 
+  return [...arr1, ...arr2].reduce((acc, i) => acc + i); 
 }
