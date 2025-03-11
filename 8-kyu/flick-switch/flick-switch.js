4a18bc984c9ecbab4537d0751387ad7f1c5c9079
Sam S. <devmansam@protonmail.com>
2025-03-11 13:33:50 -0700

    Update solution - CodeHub

--- 8-kyu/flick-switch/flick-switch.js
+++ 8-kyu/flick-switch/flick-switch.js
@@ -1,22 +1,31 @@
-function flickSwitch(arr){
-  // create a new array to hold results
-  let result = []; 
+// function flickSwitch(arr){
+//   // create a new array to hold results
+//   let result = []; 
   
-  // set the state to true initially
-  let switchState = true;
+//   // set the state to true initially
+//   let switchState = true;
   
-  // loop through array and toggle switch state when "flick" is encountered
-  for(let i = 0; i < arr.length; i++) {
+//   // loop through array and toggle switch state when "flick" is encountered
+//   for(let i = 0; i < arr.length; i++) {
     
-  // if the present index's value is "flick" then toggle state
-    if(arr[i] === "flick") {
-    switchState = !switchState;
-    } 
+//   // if the present index's value is "flick" then toggle state
+//     if(arr[i] === "flick") {
+//     switchState = !switchState;
+//     } 
     
-  // push the switch state to result array
-      result.push(switchState);
-  } 
+//   // push the switch state to result array
+//       result.push(switchState);
+//   } 
   
-  // return result array
-    return result;
+//   // return result array
+//     return result;
+// }
+​
+let flickSwitch = arr => {
+  // set initial state to true
+  let state = true;
+  // map through array and return a new array 
+  // if current index value is 'flick' then flip state (false)
+  // else state is true
+  return arr.map((i) => i === 'flick' ? state = !state : state)
 }
