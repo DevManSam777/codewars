4aa60ad428e14d9669440ed1c87d6a704c6ab9eb
Sam S. <devmansam@protonmail.com>
2025-03-13 12:49:26 -0700

    Update solution - CodeHub

--- 8-kyu/sum-mixed-array/sum-mixed-array.js
+++ 8-kyu/sum-mixed-array/sum-mixed-array.js
@@ -1,6 +1,5 @@
 function sumMix(x){
-  // iterate over x array using map method
-  // use Number function to convert all indices to numbers
   // use reduce method to add up the sum of the numbers
- return  x.map(num => Number(num)).reduce((a,b) => a + b)
+  // use Number function to convert all indices to numbers
+  return  x.reduce((a,b) => a + Number(b), 0)
 }
