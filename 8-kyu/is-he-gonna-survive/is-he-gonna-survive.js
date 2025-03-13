87e3882c1d54fee0aa72f16a015406aecee8e0af
Sam S. <devmansam@protonmail.com>
2025-03-13 12:52:20 -0700

    Add solution - CodeHub

---
+++ 8-kyu/is-he-gonna-survive/is-he-gonna-survive.js
@@ -0,0 +1,7 @@
+function hero(bullets, dragons){
+  // if the amount of bullets is greater than or equal to two times the number of dragons
+  // return true
+  // else return false
+  return bullets >= dragons * 2 ? true : false
+}
+​
