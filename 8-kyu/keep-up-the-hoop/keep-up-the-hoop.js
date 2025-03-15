2758b8f0db7292a449a194e9b96542c1f655726f
Sam S. <devmansam@protonmail.com>
2025-03-15 11:48:18 -0700

    Add solution - CodeHub

---
+++ 8-kyu/keep-up-the-hoop/keep-up-the-hoop.js
@@ -0,0 +1,8 @@
+function hoopCount (n) {
+  // use ternary operation and return 
+  // if n is greater than or equal to ten, "Greet..."
+  // else "Keep at it..."
+   return n >= 10 
+    ? "Great, now move on to tricks"
+    : "Keep at it until you get it"
+}
