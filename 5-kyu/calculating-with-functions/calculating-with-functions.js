cac25900fa61f74c863d12c73ae27a51215da729
Sam S. <devmansam@protonmail.com>
2025-03-15 18:41:02 -0700

    Update solution - CodeHub

--- 5-kyu/calculating-with-functions/calculating-with-functions.js
+++ 5-kyu/calculating-with-functions/calculating-with-functions.js
@@ -1,3 +1,36 @@
+// currying 
+// function for the number zero
+// if called alone like zero(), it returns 0
+// when given an operation like zero(plus(5)), it passes 0 as the left operand, and calls plus(rightOperand)
+function zero(operation) {
+  return operation ? operation(0) : 0
+}
+​
+function one(operation) {
+  return operation ? operation(1) : 1
+}
+​
+function two(operation) {
+  return operation ? operation(2) : 2
+}
+​
+function three(operation) {
+  return operation ? operation(3) : 3
+}
+​
+function four(operation) {
+  return operation ? operation(4) : 4
+}
+​
+function five(operation) {
+  return operation ? operation(5) : 5
+}
+​
+function six(operation) {
+  return operation ? operation(6) : 6
+}
+​
+function seven(operation) {
   return operation ? operation(7) : 7
 }
 ​
@@ -20,31 +53,3 @@ function plus(rightOperand) {
 ​
 function minus(rightOperand) {
   return function(leftOperand) {
-    return leftOperand - rightOperand;
-  }
-}
-​
-function times(rightOperand) {
-  return function(leftOperand) {
-    return leftOperand * rightOperand;
-  }
-}
-​
-function dividedBy(rightOperand) {
-  return function(leftOperand) {
-    return Math.floor(leftOperand / rightOperand);
-  }
-}
-  
-​
-function times(rightOperand) {
-  return function(leftOperand) {
-    return leftOperand * rightOperand
-  }
-}
-​
-function dividedBy(rightOperand) {
-  return function(leftOperand){
-    return Math.floor(leftOperand / rightOperand);
-  }  
-}
