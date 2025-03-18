8377ec9daa60255d8bb121929ff361aa688ad60d
Sam S. <devmansam@protonmail.com>
2025-03-17 21:07:50 -0700

    Update solution - CodeHub

--- 7-kyu/highest-and-lowest/highest-and-lowest.js
+++ 7-kyu/highest-and-lowest/highest-and-lowest.js
@@ -1,8 +1,5 @@
 function highAndLow(numbers){
-  // create a new array w/ split method
-      const numArray = numbers.split(" ")
-  // sort from high to low
-      .sort((a, b) => b - a);
-  // return first (highest num) and last indices (lowest num)
-    return `${numArray[0]} ${numArray[numArray.length - 1]}`;
+   let numArr = numbers.split(" ").sort((a,b) => b - a);
+  return `${numArr[0]} ${numArr[numArr.length - 1]}`
+    
 }
