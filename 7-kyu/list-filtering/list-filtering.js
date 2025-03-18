6e13f420b1e2b0ada2e1a1371a6eb2a7119afafb
Sam S. <devmansam@protonmail.com>
2025-03-17 20:45:46 -0700

    Update solution - CodeHub

--- 7-kyu/list-filtering/list-filtering.js
+++ 7-kyu/list-filtering/list-filtering.js
@@ -1,14 +1,6 @@
 function filter_list(l) {
   // Return a new array with the strings filtered out
-  // initialize new empty array
-  const integerArray = [];
-  // iterate through the l array
-  for (let i = 0; i < l.length; i++) {
-  // use typeof to determine if l[i] is a number
-    if (typeof l[i] === "number") {
-  // push integers to new array
-      integerArray.push(l[i]);
-    } 
-  // return new array
-  } return integerArray;
+  return l.filter((a) => {
+    return typeof a === "number"
+  })
 }
