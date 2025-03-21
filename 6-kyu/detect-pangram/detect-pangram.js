d87f2f3e491b86e8ec5027dde4309068caca3842
Sam S. <devmansam@protonmail.com>
2025-03-20 22:25:35 -0700

    Add solution - CodeHub

---
+++ 6-kyu/detect-pangram/detect-pangram.js
@@ -0,0 +1,10 @@
+function isPangram(string){
+  // normalize string by making lowercase
+  // use replace method and replace any character that is not a letter with an empty string
+  let lowerString = string.toLowerCase().replace(/[^a-z]/gi, "")
+  // convert to set to remove dupes
+  const lettersSet = new Set(lowerString);
+  // .size for sets is equivalent to .length for arrays
+  // if the size of the letters array is 26 return true
+  return lettersSet.size === 26 
+}
