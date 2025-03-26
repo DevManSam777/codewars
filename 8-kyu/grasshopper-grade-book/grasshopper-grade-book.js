ecc84cfabb4be72339dbf45cbf710bb4f663b50a
Sam S. <devmansam@protonmail.com>
2025-03-26 02:35:03 -0700

    Update solution - CodeHub

--- 8-kyu/grasshopper-grade-book/grasshopper-grade-book.js
+++ 8-kyu/grasshopper-grade-book/grasshopper-grade-book.js
@@ -1,12 +1,13 @@
 function getGrade (s1, s2, s3) {
   const average = (s1 + s2 + s3)/3;
-  return average >= 90 && average <= 100
+  return average >= 90
   ? 'A'
-    : average >= 80 && average <= 90
+    : average >= 80
   ? 'B'
-    : average >= 70 && average <= 80
+    : average >= 70
   ? 'C'
-    : average >= 60 && average <= 70
+    : average >= 60
   ? 'D'
     : 'F'
 };
+​
