8e4b284d935650bf8118aef4d1b7d09e31c3bd1e
Sam S. <devmansam@protonmail.com>
2025-04-01 16:26:06 -0700

    Update solution - CodeHub

--- 6-kyu/multiplication-table/multiplication-table.js
+++ 6-kyu/multiplication-table/multiplication-table.js
@@ -1,26 +1,17 @@
 function multiplicationTable(size) {
-  // initialize an empty array to hold the rows of the multiplication table.
-  let outerArr = [];
-​
-  // iterate through each row of the table.
-  for (let i = 0; i < size; i++) {
-    // initialize an empty array to hold the values for the current row.
-    let innerArr = [];
-​
-    // iterate through each column of the current row.
+  // initialize an outer array to hold inner arrays
+  let outerArray = [];
+  // use a for loop that iterates from 0 to size value
+  for (let i = 0; i < size; i ++) {
+  // initialize an inner arrays to hold size number of values
+    let innerArray = [];
+    // use an inner for loop from 0 to size value to add values to each inner array
     for (let j = 0; j < size; j++) {
-      // calculate the product of (row number) * (column number).
-      // add 1 to i and j because we want the table to start from 1, not 0
-      let product = (i + 1) * (j + 1);
-​
-      // add the calculated product to the current row's array
-      innerArr.push(product);
-    }
-​
-    // add the completed row (innerArr) to the outer array
-    outerArr.push(innerArr);
-  }
-​
-  // return 
-  return outerArr;
+    // add 1 to i and j because we want the table to start from 1, not 0
+      // push the the product to the current row's array
+      innerArray.push((i + 1) * (j + 1));
+      // push each inner array to the outer array
+    } outerArray.push(innerArray);
+    // return 
+  } return outerArray;
 }
