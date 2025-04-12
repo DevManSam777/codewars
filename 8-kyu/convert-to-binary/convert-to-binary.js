b3d0de5e0887a8555d1198c4811a665b0ce3926d
Sam S. <devmansam@protonmail.com>
2025-04-12 15:53:08 -0700

    Add solution - CodeHub

---
+++ 8-kyu/convert-to-binary/convert-to-binary.js
@@ -0,0 +1,5 @@
+function toBinary(n){
+  // use toString method with a radix arguement of two to convert to a binary string
+  // covert back to number and return
+  return Number(n.toString(2));
+}
