0edd35d316e3d16646f44be304fd6b0a4129d18d
Sam S. <devmansam@protonmail.com>
2025-08-28 20:40:28 -0700

    Update solution - CodeHub

--- 8-kyu/find-the-remainder/find-the-remainder.js
+++ 8-kyu/find-the-remainder/find-the-remainder.js
@@ -1,12 +1,4 @@
 function remainder(n, m){
   // Divide the larger argument by the smaller argument and return the remainder
-  if (n > m) {
-    return n % m;
-  } else if(n < m) {
-    return m % n;
-  } else if ((n && m) < 0 ){
-    return 0;
-  } else {
-    return NaN;
-  }
+  return n > m ? n % m : m % n
 }
