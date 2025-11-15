02f7dce5c0fb3e15fc2130454d54bae9ae97efc6
Sam S <devmansam@protonmail.com>
2025-11-15 15:58:15 -0800

    Update solution - CodeHub

--- 8-kyu/return-the-day/return-the-day.js
+++ 8-kyu/return-the-day/return-the-day.js
@@ -1,27 +1,12 @@
 function whatday(num) { 
 ​
-  // initialize an object of the days of the week starting with sunday.  
-  // start the keys with 1: "Sunday", incrementing to 7: "Saturday"
-  const days = {
-    1: "Sunday",
-    2: "Monday",
-    3: "Tuesday",
-    4: "Wednesday",
-    5: "Thursday",
-    6: "Friday",
-    7: "Saturday"
-  }
+  // initialize an array with the days of the week from Sunday through Saturday
   
-    // if the days object has the property of the number arguement
-    // return the value of the key equal to that number 
-    // else  "Wrong, please enter a number between 1 and 7"
+  let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+​
+  // use a short circuit evaluation to return the index of the array at num value - 1
+  // or return "Wrong, please enter a number between 1 and 7";
   
-    if (days.hasOwnProperty(num)) {
-      return days[num]
-    } else {
-      return "Wrong, please enter a number between 1 and 7";
-    }
-​
-​
+  return week[num - 1] || "Wrong, please enter a number between 1 and 7"
 ​
 }
