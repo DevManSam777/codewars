b6e5f4caf15455ef3c252d74a049a2cadf0944d1
Sam S <devmansam@protonmail.com>
2025-11-16 22:02:55 -0800

    Add solution - CodeHub

---
+++ 8-kyu/regular-ball-super-ball/regular-ball-super-ball.js
@@ -0,0 +1,6 @@
+var Ball = function(ballType) {
+   // construct a new Ball object
+   // The type of ball optional, defaults to "regular"
+  // If an argument is provided, use it; otherwise, default to "regular"
+    this.ballType = ballType || "regular";
+};
