28b3a7a84c0e064b694271d6bcafff5777e55684
Sam S <devmansam@protonmail.com>
2025-11-16 21:53:14 -0800

    Update solution - CodeHub

--- 8-kyu/reversed-strings/reversed-strings.js
+++ 8-kyu/reversed-strings/reversed-strings.js
@@ -1,11 +1,6 @@
 function solution(str){
-  // initialize an variable with the value of an empty string
-  let newStr = ""
-  // run a for loop that iterates from the last index of the string
-  // and decrements by one, appending the value of the current index
-  // to the new string variable
-  // then return the string
-  for(let i = str.length -1; i >= 0; i--) {
-    newStr += str[i]
-  } return newStr;
+  // use the split method to convert to an array
+  // use the reverse method to reverse the characters with the array
+  // use the join method to convert back into a string and return
+  return str.split('').reverse().join('');
 }
