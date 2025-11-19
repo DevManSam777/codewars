d14fe31c49a456bb7e2e46b55277f8f6a38ee921
Sam S <devmansam@protonmail.com>
2025-11-19 12:43:11 -0800

    Update solution - CodeHub

--- 7-kyu/is-this-a-triangle/is-this-a-triangle.js
+++ 7-kyu/is-this-a-triangle/is-this-a-triangle.js
@@ -1,13 +1,14 @@
 function isTriangle(a,b,c)
 {
-  // check for non-positive sides
-   if (a <= 0 || b <= 0 || c <= 0) {
-    return false; 
-  }
+  // note: the two legs must be larger than the hypotenuse
+  //  (leg1 + leg2 >= hypotenuse)
+  // first check to see that all of the sides are positive
+  // we need to add a+b and see if they are larger than c,
+  // next we need to see if a+c is larger than be
+  // nest we need to see if b+c is larger than a
+  // if these are ALL true than we need to return true
+  // else false
   
-   // not a valid triangle check.  a valid triangle would be ex. a + b > c
-   if (a + b <= c || a + c <= b || b + c <= a) {
-    return false;
-  }
-   return true
+  return a > 0 && b > 0 && c > 0 && 
+         a + b > c && a + c > b && b + c > a;
 }
