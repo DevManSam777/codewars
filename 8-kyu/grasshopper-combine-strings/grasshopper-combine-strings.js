6555ebb10d6b1226442e5f739c735ccdf6e99fd0
Sam S <devmansam@protonmail.com>
2025-11-19 23:32:41 -0800

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-combine-strings/grasshopper-combine-strings.js
@@ -0,0 +1,8 @@
+// Create the combineNames function here
+​
+// function takes 2 params - firstName lastName and returns fullName
+// ex. fistName = John lastName = Doe fullName = John Doe
+​
+function combineNames(firstName, lastName){
+  return `${firstName} ${lastName}`;
+}
