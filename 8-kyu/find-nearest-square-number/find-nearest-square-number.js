c57bcfd2a0a4090b22f8f2915f6b5971422a8a86
Sam S <devmansam@protonmail.com>
2025-11-29 19:29:18 -0800

    Update solution - CodeHub

--- 8-kyu/find-nearest-square-number/find-nearest-square-number.js
+++ 8-kyu/find-nearest-square-number/find-nearest-square-number.js
@@ -1,9 +1,3 @@
 function nearestSq(n){
-   let squareRoot = Math.sqrt(n);
-   let floor = Math.floor(squareRoot);
-   let ceiling = Math.ceil(squareRoot);
-   let floorDiff = squareRoot - floor;
-   let ceilDiff = ceiling - squareRoot;
-  
-  return ceilDiff < floorDiff ? ceiling**2 : floor**2
+    return Math.pow(Math.round(Math.sqrt(n)), 2);
 }
