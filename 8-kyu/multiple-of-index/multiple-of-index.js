18a1a8cc5235a2898402dec7901be23de61ddfaf
Sam S <devmansam@protonmail.com>
2025-12-03 03:04:52 -0800

    Update solution - CodeHub

--- 8-kyu/multiple-of-index/multiple-of-index.js
+++ 8-kyu/multiple-of-index/multiple-of-index.js
@@ -1,10 +1,4 @@
 function multipleOfIndex(array) {
-  let newArray = [];
- for (let i = 0; i < array.length; i++){
-   if (array[i] % i === 0){
-     newArray.push(array[i]);
-   } else if (array[i] === 0) {
-     newArray.push(0);
-   }
- } return newArray;
+  // good luck
+  return array.filter((num, index)=> num === 0 || num % index === 0)
 }
