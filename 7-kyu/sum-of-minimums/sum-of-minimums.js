8ea4429d63ea992a074c2adec3258beea4d13da6
Sam S <devmansam@protonmail.com>
2025-12-05 13:42:54 -0800

    Add solution - CodeHub

---
+++ 7-kyu/sum-of-minimums/sum-of-minimums.js
@@ -0,0 +1,11 @@
+function sumOfMinimums(arr) {
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++) {
+    let min = arr[i][0];
+    for(let j = 0; j < arr[i].length; j++){
+      if(arr[i][j] < min){
+        min = arr[i][j];
+      }
+    } sum += min;
+  } return sum;
+}
