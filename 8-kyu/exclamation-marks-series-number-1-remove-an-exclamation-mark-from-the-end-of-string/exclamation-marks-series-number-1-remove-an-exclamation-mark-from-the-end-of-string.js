bcf935d63be92322078133cd5ec28f18149fe04d
Sam S <devmansam@protonmail.com>
2025-12-06 14:46:37 -0800

    Update solution - CodeHub

--- 8-kyu/exclamation-marks-series-number-1-remove-an-exclamation-mark-from-the-end-of-string/exclamation-marks-series-number-1-remove-an-exclamation-mark-from-the-end-of-string.js
+++ 8-kyu/exclamation-marks-series-number-1-remove-an-exclamation-mark-from-the-end-of-string/exclamation-marks-series-number-1-remove-an-exclamation-mark-from-the-end-of-string.js
@@ -1,16 +1,3 @@
 function remove (string) {
-  // convert to an array,
-  // use a while loop to check if the last index is "!"
-  // if it is then pop it off
-  // convert back to string and return
-  
-  let hiArray = string.split('');
-  console.log(hiArray)
-  if (hiArray[hiArray.length - 1] === '!') {
-    hiArray.pop()
-  } 
-  console.log(hiArray)
-​
-  return(hiArray.join(''))
+  return string.replace(/!$/, '');
 }
-​
