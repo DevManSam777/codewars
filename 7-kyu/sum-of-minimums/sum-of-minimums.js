8d2d52ab1f345545449158588828749428a30bc4
Sam S <devmansam@protonmail.com>
2025-12-05 16:39:34 -0800

    Update solution - CodeHub

--- 7-kyu/sum-of-minimums/sum-of-minimums.js
+++ 7-kyu/sum-of-minimums/sum-of-minimums.js
@@ -1,11 +1,5 @@
-function sumOfMinimums(arr) {
-  let sum = 0;
-  for(let i = 0; i < arr.length; i++) {
-    let min = arr[i][0];
-    for(let j = 0; j < arr[i].length; j++){
-      if(arr[i][j] < min){
-        min = arr[i][j];
-      }
-    } sum += min;
-  } return sum;
-}
+ function sumOfMinimums(arr) {
+    return arr.reduce((acc, currentVal) => {
+      return acc + Math.min(...currentVal);
+    }, 0);
+  }
