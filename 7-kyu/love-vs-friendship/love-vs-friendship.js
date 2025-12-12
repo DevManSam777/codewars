b4e2544f5396db1e461fbe0f89bfdc3bece72181
Sam S <devmansam@protonmail.com>
2025-12-12 04:41:31 -0800

    Update solution - CodeHub

--- 7-kyu/love-vs-friendship/love-vs-friendship.js
+++ 7-kyu/love-vs-friendship/love-vs-friendship.js
@@ -1,19 +1,5 @@
-​
-function wordsToMarks(string){
-  let array = string.split('')
-  let alphaArray = []
-  for(let i = 97; i <= 122; i++){
-    alphaArray.push(String.fromCharCode(i))
-  }
-  console.log(alphaArray);
-  let alphaObj = alphaArray.reduce((acc, current, index)=> {
-    acc[current] = index + 1;
-    return acc; 
-}, {})
-​
-console.log(alphaObj)
-​
-return string.toLowerCase().split('').reduce((sum, letter) => sum + (alphaObj[letter] || 0), 0)
-​
+function wordsToMarks(string) {
+  return string.split('').reduce((acc, char) => {
+    return acc + char.charCodeAt() - 96
+  }, 0)
 }
-​
