16aa6485997b2271b999d1d4aa0bb79fc0dc5335
Sam S <devmansam@protonmail.com>
2025-12-12 22:28:39 -0800

    Add solution - CodeHub

---
+++ 8-kyu/who-ate-the-cookie/who-ate-the-cookie.js
@@ -0,0 +1,14 @@
+function cookie(x){
+  let name;
+  switch(typeof x){
+      case 'string':
+        name = 'Zach';
+        break;
+      case 'number':
+        name = 'Monica';
+        break;
+      default:
+        name = 'the dog'
+  } 
+  return `Who ate the last cookie? It was ${name}!`
+}
