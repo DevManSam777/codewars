287796d456412689da0cfc0fab793321e7f74441
Sam S <devmansam@protonmail.com>
2025-12-17 02:04:39 -0800

    Update solution - CodeHub

--- 6-kyu/count-the-smiley-faces/count-the-smiley-faces.js
+++ 6-kyu/count-the-smiley-faces/count-the-smiley-faces.js
@@ -1,12 +1,5 @@
 //return the total number of smiling faces in the array
 function countSmileys(arr) {
-  let smiley = 0;
-//   let regex = /^(:|;)(-*|~*)(\)|D)/
-  let regex = /^(:|;)(-{0,1}|~{0,1})(\)|D)/
-  for(let i = 0; i < arr.length; i++){
-    if (regex.test(arr[i])){
-      smiley++
-    }
-  }
-  return smiley;
+  let regex = /^(:|;)(-|~)?(\)|D)/
+  return arr.reduce((acc,current) => acc + regex.test(current), 0)
 }
