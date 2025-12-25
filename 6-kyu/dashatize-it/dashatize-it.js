5312f9464055d1d93d7bd2e81e4be805f90e7ea7
Sam S <devmansam@protonmail.com>
2025-12-25 04:13:40 -0800

    Update solution - CodeHub

--- 6-kyu/dashatize-it/dashatize-it.js
+++ 6-kyu/dashatize-it/dashatize-it.js
@@ -1,17 +1,6 @@
 function dashatize(num) {
-    let arr = String(num).replace(/[13579]/g, (n)=> `-${n}-`).split('')
-    console.log(arr);
-​
-    arr = arr.map((x, i) => {
-        if((i === 0 && x === '-') || (i === arr.length -1 && x === '-')){
-            return ''
-        } else if (x === '-' && arr[i - 1] === '-' ){
-            return ''
-        } else {
-            return x;
-        }
-    })
-    console.log(arr.join(''));
-    return arr.join('');
+    return String(num)
+    .replace(/([13579])/g, '-$1-')
+    .replace(/--+/g, '-')
+    .replace(/^-|-$/g, '')
 }
-​
