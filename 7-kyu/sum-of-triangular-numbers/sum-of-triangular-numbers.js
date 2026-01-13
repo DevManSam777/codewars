ebe128021779b5d8b07aaa9a4ef537a71e430136
Sam S <devmansam@protonmail.com>
2026-01-13 03:16:24 -0800

    Update solution - CodeHub

--- 7-kyu/sum-of-triangular-numbers/sum-of-triangular-numbers.js
+++ 7-kyu/sum-of-triangular-numbers/sum-of-triangular-numbers.js
@@ -1,4 +1,13 @@
 function sumTriangularNumbers(n) {
-  if (n < 0) return 0;
-  return n * (n + 1) * (n + 2) / 6
+  if(n < 0) return 0;
+​
+  let sum = 0;
+    
+  for(let i = 0, j = 1; i < n; i++, j+= i + 1){
+    sum += j;   
+    console.log(sum) 
+  }
+​
+  return sum
 }
+​
