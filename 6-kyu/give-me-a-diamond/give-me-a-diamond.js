b3727611527db4e2dadef7b59da95e712b4ad931
Sam S <devmansam@protonmail.com>
2026-01-17 16:29:17 -0800

    Update solution - CodeHub

--- 6-kyu/give-me-a-diamond/give-me-a-diamond.js
+++ 6-kyu/give-me-a-diamond/give-me-a-diamond.js
@@ -1,52 +1,23 @@
 function diamond(n){
-  /*
-  *   param - {number}
-  *   return - {string}
-  *   
-  *   Problem:
-  *   Given a number (n) resenting the number of * at the center of the diamond
-  *   return a string, representing a diamond
-  *       - the first row should start with 1 *
-  *       - the final row should also only have 1 *
-  *       - the number of * should decrement by 2 depending on their distance from
-  *         from the center
-  *       - each row should end with \n
-  * 
-  *    
-  *      
-  *    each besides the center row will have spaces before the *s
-  *       - the number of spaces will be determined by the distance from the center row
-  *       - spaces = Math.max(centerIndex, currentIndex) - Math.floor(currentIndex, centerIndex) 
-  *
-  *     1) make center variable and set its value to Center Row Index = Math.floor(n/2)
-  *     
-  *     n = input value
-  *     distanceFromCenter = (Math.max(centerIndex, currentIndex) - Math.floor(currentIndex, centerIndex))
-  *     stars in row = n - (2 * distanceFromCenter) 
-  *     spaces = distanceFromCenter
-  *     
-  *     spaces for each iteration ' '.repeat(distancFromCenter)
-  *     stars for each iteration '*'.(2 * distanceFromCenter)
-  *     /n
-  *     string = ''
-  *
-  *     string += spacesPer + starsPer + /n
-  *
-  *     if n < 1 or n % 2 === 0 return null
-  */
+  // first must determine centerIndex
+  // center index = Math.floor(n/2)
+  // spaces are equal to distance from center 
+  // distance from center is equal to absolute value of center index - current index
+  // number of stars decrements by 2 per 1 row distance from center
+  // stars = n - (distance from center + spaces)
   
-        
-        let string = '';
+  // 1) initialize centerIndex variable
+        const centerIndex = Math.floor(n/2)
+  // 2) initialize string variable
+        let string = ''
+  // 3) run for loop with 
+  //    counter i set to 0
+  //    condition - i < n
+  //    iterator  - i++
         for(let i = 0; i < n; i++){
-        //let currentIndex = i 
-          let centerIndex = Math.floor(n/2)
-        //let distanceFromCenter = Math.max(centerIndex, currentIndex) - Math.min(currentIndex, centerIndex)
-          let distanceFromCenter = Math.abs(centerIndex - i);
-          let spacesPer = ' '.repeat(distanceFromCenter);
-          let starsPer = '*'.repeat(n - (2 * distanceFromCenter))
-          string += spacesPer + starsPer + '\n'
+          const spaces = Math.abs(centerIndex - i);
+          const stars = n - (spaces * 2)
+          string += ' '.repeat(spaces) + '*'.repeat(stars) + '\n'
         }
-        console.log(string)
-        return n < 1 || n % 2 === 0 ? null : string
-      
+    return n < 1 || n % 2 === 0 ? null : string
 }
