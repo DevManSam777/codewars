f5e69aa688e7784edcdc86dcff93ea0d6570dc4c
Sam S <devmansam@protonmail.com>
2026-01-20 04:21:33 -0800

    Update solution - CodeHub

--- 6-kyu/detect-pangram/detect-pangram.js
+++ 6-kyu/detect-pangram/detect-pangram.js
@@ -3,7 +3,7 @@ function isPangram(string){
   // use replace method and replace any character that is not a letter with an empty string
   let lowerString = string.toLowerCase().replace(/[^a-z]/gi, "")
   // convert to set to remove dupes
-  const lettersSet = new Set(lowerString);
+  const lettersSet = new Set([...lowerString]);
   // .size for sets is equivalent to .length for arrays
   // if the size of the letters array is 26 return true
   return lettersSet.size === 26 
