a30665f184e4e14edbe474400b505b2d0c061048
Sam S <devmansam@protonmail.com>
2026-01-20 04:16:00 -0800

    Update solution - CodeHub

--- 7-kyu/indexed-capitalization/indexed-capitalization.js
+++ 7-kyu/indexed-capitalization/indexed-capitalization.js
@@ -1,12 +1,7 @@
 function capitalize(string, indices) {
-    
   const stringIndices = string.length - 1
-  console.log(stringIndices)   
-  
-  let arr = string.split('')
-  console.log(arr)
-  
-  
-  indices.filter(i => i <= stringIndices).forEach(i=> arr.splice(i, 1, arr[i].toUpperCase()))
+  const arr = string.split('')
+  indices.filter(i => i <= stringIndices)
+    .forEach(i=> arr.splice(i, 1, arr[i].toUpperCase()))
   return arr.join('')
 }
