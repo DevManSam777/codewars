13147c6043218381da58d9e822316dc8861da5ca
Sam S <devmansam@protonmail.com>
2026-01-19 17:33:30 -0800

    Update solution - CodeHub

--- 6-kyu/decipher-this/decipher-this.js
+++ 6-kyu/decipher-this/decipher-this.js
@@ -1,23 +1,8 @@
 function decipherThis(str) {
-  return str.split(' ').map(word => {
-    // extract the number at the start and the remaining letters
-    let num = word.match(/^\d+/)[0];
-    let letters = word.slice(num.length);
-​
-    // convert number to first letter
-    let first = String.fromCharCode(num);
-​
-    // swap second and last if there are 2+ letters
-    if (letters.length >= 2) {
-      let second = letters[0];
-      let middle = letters.slice(1, -1);
-      let last = letters[letters.length - 1];
-      return first + last + middle + second;
-    }
-​
-    // 0-1 letters, no swap needed
-    return first + letters;
-  }).join(' ');
+  return str.split(" ")
+    .map(w =>
+      w.replace(/^\d+/, c => String.fromCharCode(c))
+       .replace(/^(.)(.)(.*)(.)$/, "$1$4$3$2")
+    )
+    .join(" ")
 }
-​
-​
