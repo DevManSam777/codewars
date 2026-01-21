dfd0f5b36ebb4a4214e9a734420d7c5b224f510f
Sam S <devmansam@protonmail.com>
2026-01-21 01:27:06 -0800

    Update solution - CodeHub

--- 5-kyu/whats-a-perfect-power-anyway/whats-a-perfect-power-anyway.js
+++ 5-kyu/whats-a-perfect-power-anyway/whats-a-perfect-power-anyway.js
@@ -1,19 +1,9 @@
-         - i ++ 
-            - if curr to the power of i is equal to n
-            - push curr,i to acc
-            - outside loop return acc
-​
-      5) if the length of the reduced array is greatr than 1 
-         - return array
-         - else return null
- */
+function isPP(n){
   const squareRoot = Math.sqrt(n)
   if(n < 4) return null;
   if (Number.isInteger(squareRoot)){
-   // console.log([squareRoot, 2])
     return [squareRoot, 2]
   }
-//   console.log('Math.floor(squareroot)', Math.floor(squareRoot))
   let factors = []
   let i = 2;
 ​
@@ -23,8 +13,7 @@
     }
     i++;
   }
-   console.log(factors)
-​
+   
 let pows = factors.reduce((acc,curr) => {
       for(let i = 2; i <= n; i++){
          if(curr**i === n){
@@ -39,4 +28,3 @@ let pows = factors.reduce((acc,curr) => {
    return null;
   }
 }
-​
