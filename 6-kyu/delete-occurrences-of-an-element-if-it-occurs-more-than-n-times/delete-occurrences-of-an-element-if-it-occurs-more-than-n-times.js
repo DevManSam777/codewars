291a20686adaac3d47e67ddc16d046cf4ad3766d
Sam S <devmansam@protonmail.com>
2026-01-22 13:31:21 -0800

    Update solution - CodeHub

--- 6-kyu/delete-occurrences-of-an-element-if-it-occurs-more-than-n-times/delete-occurrences-of-an-element-if-it-occurs-more-than-n-times.js
+++ 6-kyu/delete-occurrences-of-an-element-if-it-occurs-more-than-n-times/delete-occurrences-of-an-element-if-it-occurs-more-than-n-times.js
@@ -1,7 +1,7 @@
-function deleteNth(arr, n){
+function deleteNth(arr,n){
   const counts = {};
-  return arr.filter(val => {
-    counts[val] = (counts[val] || 0) + 1;
-    return counts[val] <= n;
-  });
+  return arr.filter(num => {
+    counts[num] = (counts[num] || 0) + 1
+    return counts[num] <= n;
+  })
 }
