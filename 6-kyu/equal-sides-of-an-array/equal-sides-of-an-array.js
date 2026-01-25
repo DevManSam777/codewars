3d3325d6e26a5128aa5fc4fb2d53951b38608349
Sam S <devmansam@protonmail.com>
2026-01-25 00:06:48 -0800

    Update solution - CodeHub

--- 6-kyu/equal-sides-of-an-array/equal-sides-of-an-array.js
+++ 6-kyu/equal-sides-of-an-array/equal-sides-of-an-array.js
@@ -1,27 +1,6 @@
 function findEvenIndex(arr) {
-  let index = -1
-  const array = [...arr];
-        
-        if(array.length < 1 || (arr.length === 2 && (arr[0] !== 0 && arr[1] !== 0 ))){
-          return -1
-        }
-        if(array.length === 2 && array[0] === 0) {
-          return 1;
-        }
-         if((array.length === 2 && array[1] === 0) || array.length === 1) {
-          return 0;
-        }
-  
-      for(let i = 0; i < array.length; i++){
-        let left = array.slice(0,i).reduce((acc,curr)=> acc + curr, 0);
-        let right = array.slice(i+1).reduce((acc,curr)=> acc + curr, 0);
-        if(left === 'undefined' && right === 0 || right === 'undefined' && left === 0){
-          index = i;
-          break;
-        }
-        if(left === right){
-          index = i;
-          break;
-        }
-      } return index
+  return arr.findIndex((_,i) => arr.slice(0,i)
+                       .reduce((acc,curr)=> acc + curr, 0) === arr
+                       .slice(i+1)
+                       .reduce((acc,curr)=> acc + curr, 0))
 }
