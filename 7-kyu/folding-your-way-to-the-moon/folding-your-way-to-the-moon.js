ea21486a395ed11c4dbc2b86878807165866bbab
Sam S <devmansam@protonmail.com>
2026-01-29 01:08:45 -0800

    Add solution - CodeHub

---
+++ 7-kyu/folding-your-way-to-the-moon/folding-your-way-to-the-moon.js
@@ -0,0 +1,15 @@
+function foldTo(distance) {
+  if (distance < 0) {
+    return null;
+  }
+  
+  let thickness = 0.0001;
+  let folds = 0;
+  
+  while (thickness < distance) {
+    thickness *= 2; 
+    folds++;
+  }
+  
+  return folds;
+}
