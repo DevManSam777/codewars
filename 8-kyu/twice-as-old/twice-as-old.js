5bc6d3cba0aa38d7ef4528979002f442e6636f1c
Sam S <devmansam@protonmail.com>
2026-01-31 00:33:52 -0800

    Update solution - CodeHub

--- 8-kyu/twice-as-old/twice-as-old.js
+++ 8-kyu/twice-as-old/twice-as-old.js
@@ -1,17 +1,4 @@
 function twiceAsOld(dadYearsOld, sonYearsOld) {
-  let yearsAgo = 0;
-  
-  if(dadYearsOld > 2 * sonYearsOld){
-    while(dadYearsOld != 2 * sonYearsOld){
-      dadYearsOld++;
-      sonYearsOld++;
-      yearsAgo++
-    } 
-  } else {
-      while(dadYearsOld < 2 * sonYearsOld){
-        dadYearsOld--;
-        sonYearsOld--;
-        yearsAgo++;
-      }
-    } return yearsAgo;
+  return Math.abs(dadYearsOld - sonYearsOld * 2)
 }
+​
