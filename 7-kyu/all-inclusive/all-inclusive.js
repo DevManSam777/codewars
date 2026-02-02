504fac2f0e3d88639e237b0f6864b7eb60737b51
Sam S <devmansam@protonmail.com>
2026-02-02 15:12:26 -0800

    Update solution - CodeHub

--- 7-kyu/all-inclusive/all-inclusive.js
+++ 7-kyu/all-inclusive/all-inclusive.js
@@ -1,12 +1,3 @@
 function containAllRots(str, arr) {
-    let arrFromStr = [];
-  
-    for(let i = 0; i < str.length; i++){
-      arrFromStr.push(str)
-      let last = str[0];
-      let rest = str.slice(1);
-      str = rest + last;
-    }
-   return arrFromStr.every(val => arr.includes(val))
+  return str.split('').every((char,i) => arr.includes(str.slice(i) + str.slice(0,i)))
 }
-​
