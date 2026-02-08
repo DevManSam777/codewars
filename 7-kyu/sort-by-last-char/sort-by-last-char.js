175d403fba4b688bfd1155c62cd31cd85a7d73a1
Sam S <devmansam@protonmail.com>
2026-02-07 19:41:02 -0800

    Update solution - CodeHub

--- 7-kyu/sort-by-last-char/sort-by-last-char.js
+++ 7-kyu/sort-by-last-char/sort-by-last-char.js
@@ -1,4 +1,3 @@
 function last(x){
-  return  x.split(' ').map(word=> word.split('').reverse().join('')).sort((a,b)=> a[0].localeCompare(b[0])).map(word=> word.split('').reverse().join(''))
+  return x.split(' ').sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
 }
-​
