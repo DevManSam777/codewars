1a5b615f4d1e413ddf752c07a97ef81e9d5097c5
Sam S <devmansam@protonmail.com>
2026-02-25 10:46:04 -0800

    Update solution - CodeHub

--- 8-kyu/training-js-number-14-methods-of-number-object-tostring-and-tolocalestring/training-js-number-14-methods-of-number-object-tostring-and-tolocalestring.js
+++ 8-kyu/training-js-number-14-methods-of-number-object-tostring-and-tolocalestring/training-js-number-14-methods-of-number-object-tostring-and-tolocalestring.js
@@ -1,5 +1,4 @@
-function colorOf(r,g,b){
-  let hex = [r,g,b].map(val => val.toString(16).padStart(2,0)).join('');
+function colorOf(...rgb){
+  let hex = rgb.map(val => val.toString(16).padStart(2,0)).join('');
   return `#${hex}`
 }
-​
