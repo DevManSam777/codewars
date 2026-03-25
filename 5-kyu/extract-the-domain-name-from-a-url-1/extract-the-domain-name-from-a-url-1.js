10a028ea1bdb35e79e1109606964bc3d6b4cedbb
Sam S <devmansam@protonmail.com>
2026-03-24 17:21:15 -0700

    Update solution - CodeHub

--- 5-kyu/extract-the-domain-name-from-a-url-1/extract-the-domain-name-from-a-url-1.js
+++ 5-kyu/extract-the-domain-name-from-a-url-1/extract-the-domain-name-from-a-url-1.js
@@ -1,14 +1,5 @@
-function domainName(url) {
-  // make regex
-  // handle urls that do not begin with // so use a non capturing group for http https & www.
-  let urlRegex = /(?:https?:\/\/)?(?:www\.)?([^.\/]+)\./;
-  // match equals the captured group
-  let match = url.match(urlRegex);
-  if (match) {
-    // return the first captured group
-    return match[1];
-  } else {
-    // or null
-    return null;
-  }
+function domainName(url){
+   const regex = /(www.|http(s)?:\/\/(www.)?|)(?<domain>[^.]+)./
+   console.log(regex.test(url))
+    return url.match(regex).groups.domain
 }
